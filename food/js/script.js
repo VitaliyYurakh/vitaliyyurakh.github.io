f54e241b386c786ddc8501ecd1a14078b4fc192e
Vitaliy Yurakh <109657047+VitaliyYurakh@users.noreply.github.com>
2023-03-13 11:16:33 +0200

    Update script.js

--- food/js/script.js
+++ food/js/script.js
@@ -12,7 +12,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
     modal('[data-modal]','.modal', modalTimerId);
-    timer('.timer', '2022-11-9');
+    timer('.timer', '2023-5-20');
     cards();
     calc();
     forms('form', modalTimerId);
