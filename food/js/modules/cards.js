f074f17a5b75a0164de301f0e14bdb4b2bb89ee2
Vitaliy Yurakh <109657047+VitaliyYurakh@users.noreply.github.com>
2023-03-22 13:17:54 +0200

    Update cards.js

--- food/js/modules/cards.js
+++ food/js/modules/cards.js
@@ -3,46 +3,46 @@ import {getResource} from '../services/services'
 function cards() {
     // Використовуем класи
 
-    class MenuCard {
-        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
-            this.src = src;
-            this.alt = alt;
-            this.title = title;
-            this.descr = descr;
-            this.price = price;
-            this.parentSelector = document.querySelector(parentSelector);
-            this.classes = classes;
-            this.transfer = 42;
-            this.changeToUAH()
-        }
+//     class MenuCard {
+//         constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+//             this.src = src;
+//             this.alt = alt;
+//             this.title = title;
+//             this.descr = descr;
+//             this.price = price;
+//             this.parentSelector = document.querySelector(parentSelector);
+//             this.classes = classes;
+//             this.transfer = 42;
+//             this.changeToUAH()
+//         }
 
-        changeToUAH() {
-            this.price = this.price * this.transfer;
-        }
+//         changeToUAH() {
+//             this.price = this.price * this.transfer;
+//         }
 
-        render() {
-            const element = document.createElement('div')
-            this.classes.forEach(className => element.classList.add(className))
-            element.innerHTML = `
-                    <img src=${this.src} alt=${this.alt}>
-                    <h3 class="menu__item-subtitle">${this.title}</h3>
-                    <div class="menu__item-descr">${this.descr}</div>
-                    <div class="menu__item-divider"></div>
-                    <div class="menu__item-price">
-                        <div class="menu__item-cost">Цена:</div>
-                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-                    </div>
-            `;
-            this.parentSelector.append(element)
-        }
-    }
+//         render() {
+//             const element = document.createElement('div')
+//             this.classes.forEach(className => element.classList.add(className))
+//             element.innerHTML = `
+//                     <img src=${this.src} alt=${this.alt}>
+//                     <h3 class="menu__item-subtitle">${this.title}</h3>
+//                     <div class="menu__item-descr">${this.descr}</div>
+//                     <div class="menu__item-divider"></div>
+//                     <div class="menu__item-price">
+//                         <div class="menu__item-cost">Цена:</div>
+//                         <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+//                     </div>
+//             `;
+//             this.parentSelector.append(element)
+//         }
+//     }
 
-    getResource('http://localhost:3000/menu')
-    .then(data => {
-        data.forEach(({img, altimg, title, descr, price}) => {
-            new MenuCard(img, altimg, title, descr, price, '.menu .container', 'menu__item').render()
-        })
-    })
+//     getResource('http://localhost:3000/menu')
+//     .then(data => {
+//         data.forEach(({img, altimg, title, descr, price}) => {
+//             new MenuCard(img, altimg, title, descr, price, '.menu .container', 'menu__item').render()
+//         })
+//     })
 
     // getResource('http://localhost:3000/menu')
     // .then(data => createCard(data))
